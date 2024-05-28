6e89495ce03662e02a48c22958209afdb1d7048c
LawanF <lawanf@users.noreply.github.com>
2024-05-28 12:11:43 +0100

    implemented dedicated filesize function, refactored file to fileio.c

---
+++ src/fileio.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Takes a FILE *fileptr and a long int *size.
+    Stores the size of file (in bytes) in size.
+*/
+void filesize(FILE *fileptr, long int *size) {
+    if (fseek(fileptr, 0, SEEK_END)) {
+        fprintf(stderr, "filesize: fseek error.");
+        exit(1);
+    }
+
+    if ((*size = ftell(fileptr)) < 0) {
+        fprintf(stderr, "filesize: ftell error.");
+        exit(1);
+    }
+
+    if (fseek(fileptr, 0, SEEK_SET)) {
+        fprintf(stderr, "filesize: fseek error.");
+        exit(1);
+    }
+}
+
+/*
+    Takes a string that specifies a file location.
+    Returns a char array with the contents of that file.
+*/
+char *store_file_to_arr(char *filename) {
+    // Define array to return.
+    char *arr;
+
+    // Open file and check if it opens successfully.
+    FILE *fileptr = fopen(filename, "r");
+    if (fileptr == NULL) {
+        fprintf(stderr, "store_file_to_arr: can’t open %s\n", filename);
+        exit(1);
+    }
+
+    // Store filesize in size.
+    long int size;
+    filesize(fileptr, &size);
+    // printf("%lx", size);
+
+    // Allocate memory to arr and check if allocation is successful.
+    arr = malloc(size);
+    if (arr == NULL) {
+        fprintf(stderr, "store_file_to_arr: ran out of memory.");
+        exit(1);
+    }
+
+    // Read file into arr.
+    fread(arr, 1, size, fileptr);
+
+    // Close file.
+    fclose(fileptr);
+
+    return arr;
+}
+
+
