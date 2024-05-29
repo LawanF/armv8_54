096d10d61ca873a3bbf6d8ae8e1371b9edd052e2
Sani <sa4523@imperial.ac.uk>
2024-05-29 19:54:49 +0100

    added dp immediate arithmetic instruction execution blueprint

--- src/instructions.c
+++ src/instructions.c
@@ -157,12 +157,96 @@ void execute(Instruction *inst) {
     if (inst == NULL) return;
     enum CommandFormat inst_command_format = inst->command_format;
     switch (inst_command_format) {
-        case HALT: {
+        
+	MachineState *machine_state = read_machine_state();
+
+	case HALT: {
 
             break;
         }
         case DP_IMM: {
+	    DPImmOperandType dpimm_operand_type = (inst->dp_imm).operand_type;
+	    char dp_imm_opc:2 = (inst->opc);
+	    char dp_imm_rd:5 = (inst->rd);
+            switch (dpimm_operand_type) {
+	    	case ARITH_OPERAND: {
+		    // get imm12, rn
+		    // get sh and see if need to left shift imm12
+		    // stack pointer (rn/rd = 11111 case handled by write to register function)
+		    // use opc to select specific instruction, update rd and condition (pstate) flags if needed
+		
+		    int32_t dp_imm_imm12 = (inst->operand).arith_operand.imm12;
+		    char dp_imm_rn:5 = (inst->operand).arith_operand.rn;
+		    char dp_imm_sh:1 = (inst->operand).arith_operand.sh;
 
+		    if (sh==1) {
+		        dp_imm_imm12 = dp_imm_imm12 << 12;
+		    }
+
+	            switch (dp_imm_opc) {
+		        case 0: {
+			    write_general_registers(dp_imm_rd, (machine_state->general_registers)[dp_imm_rn].data + dp_imm_imm12);		
+			    break;
+			}
+		        case 1: {
+			    // INT32 ? OR INT64 - HOW ARE WE HANDLING REGISTER SIZE
+			    int32_t rn_data = (machine_state->general_registers)[dp_imm_rn].data		
+			    int32_t result = rn_data + dp_imm_imm12;
+			    write_general_registers(dp_imm_rd, result);
+			    if (res < 0) {
+			        // set sign flag to 1
+			    } else {
+			        // set sign flag to 0
+			    }
+			    if (res == 0) {
+			        // set zero flag to 1
+			    } else {
+			        // set zero flag to 0
+			    }
+			    if (res < rn_data || res < dp_imm+imm12) {
+			        // set carry flag to 1
+			    } else {
+			        // set carry flag to 0
+			    }
+			    if ((rn_data > 0 && dp_imm_imm12 > 0 && res < 0) || (rn_data < 0 && dp_imm_imm12 < 0 && res > 0)) {
+				// set signed overflow flag to 1
+			    } else if ((rn_data < 0 && dp_imm_imm12 > 0 && res > 0) || (rn_data > 0 && dp_imm_imm12 < 0 && res > 0)) {
+			        // set signed overflow flag to 1
+			    } else {
+			        // set signed overflow flag to 0
+			    }
+
+			    // HOW ARE WE HANDLING SIGNED / UNSIGNED INTEGERS
+			    // figure out how we are handling the size of the register - is the result meant to be stored as 32 or 64 bit int
+			    break;
+			}
+			case 2: {
+			    write_general_registers(dp_imm_rd, (machine_state->general_registers)[dp_imm_rn].data - dp_imm_imm12);
+			    break;
+			}
+			case 3: {
+			    int32_t result = (machine_state->general_registers)[dp_imm_rn].data - dp_imm_imm12;
+                            write_general_registers(dp_imm_rd, result);
+			    break;
+			}
+		    }
+	            break;
+	        }
+	        case WIDE_MOVE_OPERAND: {
+		    switch (dp_imm_opc) {
+                        case 0: {
+                            break;
+                        }             
+                        case 2: {
+                            break;
+                        }
+                        case 3: {
+                            break;
+                        }       
+                    }
+		    break;
+		}
+	    }	    
             break;
         }
         case DP_REG: {
@@ -180,8 +264,7 @@ void execute(Instruction *inst) {
         case BRANCH: {
 	    // decrement pc when editing
 	    // how to specify PC when writing to machine state
-	    
-	    MachineState *machine_state = read_machine_state();
+	   
 
 	    enum BRANCH_OPERAND_TYPE branch_operand_type = (inst->branch).operand_type;
 
@@ -196,14 +279,14 @@ void execute(Instruction *inst) {
 			// use machine state function to read register branch_xn
 			// if xn is 11111 then ignore
 			// then write address in branch_xn to PC
-			int8_t xn = (inst->branch).operand.register_branch.xn;
-			if (xn != 31) {
-				write_machine_state(program_counter, (machine_state->general_registers)[xn]);
+			char register_branch_xn:5 = (inst->branch).operand.register_branch.xn;
+			if (register_branch_xn != 31) {
+				write_machine_state(program_counter, (machine_state->general_registers)[register_branch_xn].data);
 			}
 			break;
 		    }
 		    case COND_BRANCH: {
-			int8_t eval_cond = (inst->branch).operand.cond_branch.cond;
+			char eval_cond:4 = (inst->branch).operand.cond_branch.cond;
 			ProcessorStateRegister branch_pstate = (machine_state->pstate);
 			switch (eval_cond) {
 			    case 0: {
