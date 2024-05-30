86178aeafc7b807dfd0c17674b81e44137b71c21
LawanF <lawanf@users.noreply.github.com>
2024-05-30 18:47:15 +0100

    Implemented error checks through checkmem32. Cleaned up comments.

--- src/memory.c
+++ src/memory.c
@@ -1,29 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "memory.h"
 
-#define MEMORY_SIZE 2097152
-
 // Define a char[] representing the machine memory.
+// Data is stored in little-endian.
 static unsigned char memory[MEMORY_SIZE];
 
 /*
-    Sets all values of memory to 0.
+    Clears memory, setting all values to 0.
 */
 void initmem() {
     memset(memory, 0, MEMORY_SIZE * sizeof(char));
 }
 
 /*
-    Takes an address as uint32_t.
+    Takes a 21-bit address as uint32_t.
+    Checks if address is less than or equal to (MEMORY_SIZE - 4) and
+    if address adheres to 4-byte boundary (for readmem32 and writemem32).
+*/
+static void checkaddress32(uint32_t address) {
+    // Check if address is a multiple of 4.
+    if ((address % 4) != 0) {
+        fprintf(stderr, "checkaddress32: address %08x is not multiple of 4\n", address);
+        exit(1);
+    }
+
+    // Check if address is within bounds of memory.
+    if (address > MEMORY_SIZE - 4) {
+        fprintf(stderr, "checkaddress32: address %08x is out of bounds\n", address);
+        exit(1);
+    }
+}
+
+/*
+    Takes a 21-bit address as uint32_t.
     Returns a pointer to the byte at that address as unsigned char.
 */
 static unsigned char *fetchbyte(uint32_t address) {
+    // Check address.
+    checkaddress32(address);
+
     return &memory[address];
 }
 
 /*
-    Takes an address as uint32_t.
-    Returns 32-bits of data at that address as uint32_t.
+    Takes a 21-bit address as uint32_t.
+    Returns 32 bits (4 bytes) of data at that address as uint32_t.
 */
 uint32_t readmem32(uint32_t address) {
     // Fetch pointer to the first byte.
@@ -41,7 +64,7 @@ uint32_t readmem32(uint32_t address) {
 
 /*
     Takes an address as uint32_t.
-    Returns 64-bits of data at that address as uint64_t.
+    Returns 64 bits (8 bytes) of data at that address as uint64_t.
 */
 uint64_t readmem64(uint32_t address) {
     // Define uint64_t to return.
@@ -57,8 +80,8 @@ uint64_t readmem64(uint32_t address) {
 }
 
 /*
-    Takes an address and data as uint32_t.
-    Writes 4 bytes at specified address.
+    Takes an address and data 32 bits of data as uint32_t.
+    Writes 32 bits (4 bytes) at specified address.
 */
 void writemem32(uint32_t address, uint32_t data) {
     // Fetch pointer to the first byte.
@@ -72,8 +95,8 @@ void writemem32(uint32_t address, uint32_t data) {
 }
 
 /*
-    Takes an address as uint32_t and 64-bit data as uint64_t.
-    Writes 8 bytes at specified address.
+    Takes an address as uint32_t andi 64 bits of data as uint64_t.
+    Writes 64 bits (8 bytes) at specified address.
 */
 void writemem64(uint32_t address, uint64_t data) { 
     // Write to memory using writemem32.
@@ -81,4 +104,9 @@ void writemem64(uint32_t address, uint64_t data) {
         writemem32(address + i, data);
         data >>= 32;
     }
+}
+
+int main() {
+    writemem64(MEMORY_SIZE - 4, 0xff);
+    return 0;
 }
