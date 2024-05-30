a699c947d99879d168a5a89b0cb22e3138477aad
LawanF <lawanf@users.noreply.github.com>
2024-05-30 14:07:54 +0100

    created header file memory.h

---
+++ src/memory.h
@@ -0,0 +1,17 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+#include <stdint.h>
+
+#define MEMORY_SIZE 2097152
+
+extern void initmem();
+
+extern uint32_t readmem32(uint32_t address);
+
+extern uint64_t readmem64(uint32_t address);
+
+extern void writemem32(uint32_t address, uint32_t data);
+
+extern void writemem64(uint32_t address, uint64_t data);
+
+#endif
