edcbb2b153c54599b976dfdea4cc0eae962daaf9
Mohammad A <ma3523@ic.ac.uk>
2024-05-30 23:54:17 +0100

    corrected structure of instructions due to behaviour of bit fields
    For more information, see
    https://stackoverflow.com/questions/30614340/getting-negative-value-with-bit-fields
    When a char field 1 bit wide is set to 1, it is interpreted as -1 and not 1.
    So to correct this, this commit changes char to unsigned char.

--- src/instructions.c
+++ src/instructions.c
@@ -21,16 +21,16 @@ typedef union {
        - sh determines whether to left shift the immediate value by 12 bits
        - imm12 is an unsigned immediate value of 12 bits
        - rn is a register which is added or subtracted to for setting into rd */
-    struct { char sh:1; uint16_t imm12:12; char rn:5; } arith_operand;
+    struct { unsigned char sh:1; uint16_t imm12:12; unsigned char rn:5; } arith_operand;
     /* wide move instruction: 
        - hw determines a left shift by multiple of 16
        - imm16 is an unsigned immediate value of 16 bits */
-    struct { char hw:2; uint16_t imm16; } wide_move_operand;
+    struct { unsigned char hw:2; uint16_t imm16; } wide_move_operand;
 } DPImmOperand;
 
 typedef enum { REGISTER_OFFSET, PRE_INDEX_OFFSET, POST_INDEX_OFFSET, UNSIGNED_OFFSET } SDTOffsetType;
 typedef union {
-    char xm:5; // register offset
+    unsigned char xm:5; // register offset
     int16_t simm9:9; // pre or post index
     uint16_t imm12:12; // unsigned offset
 } SDTOffset;
@@ -38,19 +38,19 @@ typedef union {
 typedef enum { UNCOND_BRANCH, REGISTER_BRANCH, COND_BRANCH, } BranchOperandType;
 typedef union {
     struct { int32_t simm26:26; } uncond_branch;
-    struct { char xn:5; } register_branch;
-    struct { char cond:4; int32_t simm19:19; } cond_branch;
+    struct { unsigned char xn:5; } register_branch;
+    struct { unsigned char cond:4; int32_t simm19:19; } cond_branch;
 } BranchOperand;
 
 // generic instruction struct - unions for specific instruction data
 typedef struct {
     CommandFormat command_format;
     // sign flag (for all types except branch)
-    char sf:1;
+    unsigned char sf:1;
     // opcode (for DP instructions)
-    char opc:2;
+    unsigned char opc:2;
     // destination (DP) or target (SDT or load literal) register index
-    union { char rd:5; char rt:5; };
+    union { unsigned char rd:5; unsigned char rt:5; };
     union {
         // DP (immediate)
         struct { DPImmOperandType operand_type; DPImmOperand operand; } dp_imm;
@@ -59,12 +59,15 @@ typedef struct {
            - rm is the multiplier (for multiply instructions)
            - rn is the multiplicand (for multiply instructions) or the left hand side of bitwise operations
            - operand contains data such as an immediate value or shift */
-        struct { char m:1; char opr:4; char rm:5; char operand:6; char rn:5; } dp_reg;
+        struct {
+            unsigned char m:1; unsigned char opr:4; unsigned char rm:5;
+            unsigned char operand:6; unsigned char rn:5;
+        } dp_reg;
         /* single data transfer:
            - u is the unsigned offset flag
            - l determines a load rather than a store
            - xn is the base register */
-        struct { char u:1; char l:1; char xn:5;
+        struct { unsigned char u:1; unsigned char l:1; unsigned char xn:5;
             SDTOffsetType offset_type; SDTOffset offset;
         } single_data_transfer;
         // load literal: simm19 is a signed immediate value
@@ -360,9 +363,9 @@ uint32_t encode_single_data_transfer(Instruction *inst) {
 
 Instruction decode_load_literal(uint32_t inst_data) {
     int32_t simm19 = (int32_t) BITMASK(inst_data, 5, 23);
-    if (GET_BIT(simm19, 19)) {
+    if (GET_BIT(simm19, 18)) {
         // sign extend simm19: remaining bits 19-31 are 1
-        simm19 |= 0xFFF00000UL;
+        simm19 |= 0xFFF80000UL;
     }
     return (Instruction) {
         .command_format = LOAD_LITERAL,
