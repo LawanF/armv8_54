1b7a512e4915caa7b9ac6cb73027d4a7a8fd6bc8
Sani <sa4523@imperial.ac.uk>
2024-05-31 02:26:17 +0100

    dp_reg execute for arithmetic and logical instructions

--- src/instructions.c
+++ src/instructions.c
@@ -8,6 +8,8 @@
 #define GET_BIT(n, i) (((n) >> (i)) & 0x1) 
 // Applies a bitmask and returns bits from i (inclusive) to j (inclusive) of n, where i<=j
 #define BIT_MASK(n, i, j) (((n) >> (i)) & (0x1 << ((j) - (i) + 1) - 1))
+// Negates a number 32-bit number n
+#define NEGATE_MASK(n) ((n) ^ (0xffffffff))
 
 // enum for specifying type of instruction
 typedef enum { UNKNOWN; HALT; DP_IMM; DP_REG; SINGLE_DATA_TRANSFER; LOAD_LITERAL; BRANCH; } CommandFormat;
@@ -295,6 +297,80 @@ static void offset_program_counter(MachineState *alter_machine_state, int32_t en
         write_program_counter(offset);
 }
 
+static void arith_instr_exec(char opc:2, char rd:5, uint32_t rn_data, uint32_t op2) {
+	switch (opc) {
+                        case 0: {
+                            write_general_registers(rd, rn_data + op2);
+                            break;
+                        }
+                        case 1: {
+                            // INT32 ? OR INT64 - HOW ARE WE HANDLING REGISTER SIZE
+
+
+                            uint32_t res = rn_data + dp_imm_imm12;
+
+
+                            write_general_registers(rd, res);
+
+
+                            if (res < 0) {
+                                // set sign flag to 1
+                            } else {
+                                // set sign flag to 0
+                            }
+                            if (res == 0) {
+                                // set zero flag to 1
+                            } else {
+                                // set zero flag to 0
+                            }
+                            if (res < rn_data || res < dp_imm+imm12) {
+                                // set carry flag to 1
+                            } else {
+                                // set carry flag to 0
+                            }
+                            if ((rn_data > 0 && op2 > 0 && res < 0) || (rn_data < 0 && op2 < 0 && res > 0)) {
+                                // set signed overflow flag to 1
+                            } else {
+                                // set signed overflow flag to 0
+                            }
+
+                            // HOW ARE WE HANDLING SIGNED / UNSIGNED INTEGERS
+                            // figure out how we are handling the size of the register - is the result meant to be stored as 32 or 64 bit int
+                            break;
+                        }
+                        case 2: {
+                            write_general_registers(rd, rn_data - op2);
+                            break;
+                        }
+                        case 3: {
+                            // INT32 ? OR INT64 - HOW ARE WE HANDLING REGISTER SIZE
+                            uint32_t res = rn_data - op2;
+                            write_general_registers(rd, res);
+                            if (res < 0) {
+                                // set sign flag to 1
+                            } else {
+                                // set sign flag to 0
+                            }
+                            if (res == 0) {
+                                // set zero flag to 1
+                            } else {
+                                // set zero flag to 0
+                            }
+                            if (res < rn_data || res < op2) {
+                                // set carry flag to 1
+			    } else {
+                                // set carry flag to 0
+                            }
+                            if ((rn_data < 0 && op2 > 0 && res > 0) || (rn_data > 0 && op2 < 0 && res < 0)) {
+                                // set signed overflow flag to 1
+                            } else {
+                                // set signed overflow flag to 0
+                            }
+                            break;
+                        }
+                    }
+}
+
 void execute(Instruction *inst) {
     if (inst == NULL) return;
     enum CommandFormat inst_command_format = inst->command_format;
@@ -318,88 +394,18 @@ void execute(Instruction *inst) {
 		    // stack pointer (rn/rd = 11111 case handled by write to register function)
 		    // use opc to select specific instruction, update rd and condition (pstate) flags if needed
 	
-		    uint32_t dp_imm_imm12 = (inst->operand).arith_operand.imm12;
+		    uint32_t dp_imm_imm12 = (inst->dp_imm).operand.arith_operand.imm12;
 			
-		    char dp_imm_rn:5 = (inst->operand).arith_operand.rn;
-		    char dp_imm_sh:1 = (inst->operand).arith_operand.sh;
+		    char dp_imm_rn:5 = (inst->dp_imm).operand.arith_operand.rn;
+		    char dp_imm_sh:1 = (inst->dp_imm).operand.arith_operand.sh;
 
 		    if (sh==1) {
 		        dp_imm_imm12 = dp_imm_imm12 << 12;
 		    }
+		    uint32_t dp_imm_rn_data = (machine_state->general_registers)[dp_imm_rn].data;
 
-	            switch (dp_imm_opc) {
-		        case 0: {
-			    write_general_registers(dp_imm_rd, (machine_state->general_registers)[dp_imm_rn].data + dp_imm_imm12);		
-			    break;
-			}
-		        case 1: {
-			    // INT32 ? OR INT64 - HOW ARE WE HANDLING REGISTER SIZE
-			    uint64_t rn_data = (machine_state->general_registers)[dp_imm_rn].data;		
-			    
+		    arith_instr_exec(dp_imm_opc, dp_imm_rd, dp_imm_rn_data, dp_imm_imm12);
 
-			    uint64_t res = rn_data + dp_imm_imm12;
-
-			    
-			    write_general_registers(dp_imm_rd, res);
-			    
-
-			    if (res < 0) {
-			        // set sign flag to 1
-			    } else {
-			        // set sign flag to 0
-			    }
-			    if (res == 0) {
-			        // set zero flag to 1
-			    } else {
-			        // set zero flag to 0
-			    }
-			    if (res_32 < rn_data || res_32 < dp_imm+imm12) {
-			        // set carry flag to 1
-			    } else {
-			        // set carry flag to 0
-			    }
-			    if ((rn_data > 0 && dp_imm_imm12 > 0 && res_32 < 0) || (rn_data < 0 && dp_imm_imm12 < 0 && res_32 > 0)) {
-				// set signed overflow flag to 1
-			    } else {
-			        // set signed overflow flag to 0
-			    }
-
-			    // HOW ARE WE HANDLING SIGNED / UNSIGNED INTEGERS
-			    // figure out how we are handling the size of the register - is the result meant to be stored as 32 or 64 bit int
-			    break;
-			}
-			case 2: {
-			    write_general_registers(dp_imm_rd, (machine_state->general_registers)[dp_imm_rn].data - dp_imm_imm12);
-			    break;
-			}
-			case 3: {
-			    // INT32 ? OR INT64 - HOW ARE WE HANDLING REGISTER SIZE
-			    uint32_t rn_data = (machine_state->general_registers)[dp_imm_rn].data                
-                            uint32_t result = rn_data - dp_imm_imm12;
-                            write_general_registers(dp_imm_rd, result);
-                            if (res < 0) {
-                                // set sign flag to 1
-                            } else {
-                                // set sign flag to 0
-                            }
-                            if (res == 0) {
-                                // set zero flag to 1
-                            } else {
-                                // set zero flag to 0
-                            }
-                            if (res < rn_data || res < dp_imm+imm12) {
-                                // set carry flag to 1
-                            } else {
-                                // set carry flag to 0
-                            }
-                            if ((rn_data < 0 && dp_imm_imm12 > 0 && res > 0) || (rn_data > 0 && dp_imm_imm12 < 0 && res < 0)) {
-                                // set signed overflow flag to 1
-                            } else {
-                                // set signed overflow flag to 0
-                            }
-			    break;
-			}
-		    }
 	            break;
 	        }
 	        case WIDE_MOVE_OPERAND: {
@@ -409,8 +415,8 @@ void execute(Instruction *inst) {
                         // In the 32-bit version of the move instruction, hw can only take the values 00 or 01 (representing shifts of 0 or 16 bits)
                         // ^ separate 32 bit instr? do i need error checks for values that aren't 0 or 1
 
-                        uint16_t wide_move_imm16 = (inst->operand).wide_move_operand.imm16;
-                        char wide_move_hw:2 = (inst->operand).wide_move_operand.hw;
+                        uint16_t wide_move_imm16 = (inst->dp_imm).operand.wide_move_operand.imm16;
+                        char wide_move_hw:2 = (inst->dp_imm).operand.wide_move_operand.hw;
 
                         uint32_t wide_move_operand = wide_move_imm16 << (wide_move_hw * 16);
                         case 0: {
@@ -418,8 +424,7 @@ void execute(Instruction *inst) {
 			    // set all bits to one except imm16 bits (which these are will vary depending on if the imm16 was shifted earlier)
 			    // in 32 bit case upper 32 bits are all 0 (i.e. zero extended)
 			    // set rd to op
-			    uint32_t not_bit_mask = 4294967295
-			    wide_move_operand = wide_move_operand ^ not_bit_mask 
+			    wide_move_operand = NEGATE_MASK(wide_move_operand) 
 			    write_general_registers(dp_imm_rd, wide_move_hw);
                             break;
                         }             
@@ -433,11 +438,11 @@ void execute(Instruction *inst) {
 			    // and with operand bits
 			    // in 32 bit version zero extend to 64
 			    uint32_t wide_move_rd_data = (machine_state->general_registers)[dp_imm_rd].data;
-			    uint32_t operand_bit_mask = 65535;
 			    if (hw == 0) {
-			       operand_bit_mask = 4294901760;
+			       wide_move_rd_data = BIT_MASK(wide_move_rd_data, 16, 31)
+			    } else if (hw == 1) {
+			       wide_move_rd_data = BIT_MASK(wide_move_rd_data, 0, 15)
 			    }
-			    wide_move_rd_data = wide_move_rd_data & operand_bit_mask;
 			    wide_move_rd_data = wide_move_rd_data & wide_move_operand;
 			    write_general_registers(dp_imm_rd, wide_move_hw);
                             break;
@@ -449,7 +454,75 @@ void execute(Instruction *inst) {
             break;
         }
         case DP_REG: {
+	    char dp_reg_sf:1 = (inst->sf);
+	    char dp_reg_opc:2 = (inst->opc);
+	    char dp_reg_m:1 = (inst->dp_reg).m;
+	    char dp_reg_opr:4 = (inst->dp_reg).dp_reg.opr;
+	    char dp_reg_rm:5 = (inst->dp_reg).dp_reg.rm;
+	    char dp_reg_operand:6 = (inst->dp_reg).operand;
+	    char dp_reg_rn:5 = (inst->dp_reg).rn;
+	    char dp_reg_rd:5 = (inst->rd);
+	    if (dp_reg_m == 0) {
+		char dp_reg_shift = (GET_BIT(dp_reg_opr, 1) << 1) & (GET_BIT(dp_reg_opr, 2));
+		// get data from registers rn, rm
+		// perform shift on rm for cases 00, 01, 10, case 11 only done in logical case
+		// op2 = rm shifted operand many bits
+		// then execute instruction Rd = Rn (op) Op2
+		
+		uint32_t dp_reg_rn_data = (machine_state->general_registers)[dp_reg_rn].data;
+		uint32_t dp_reg_rm_data = (machine_state->general_registers)[dp_reg_rm].data;
+		
+		switch (dp_reg_shift) {
+			case 0: { dp_reg_rm_data = dp_reg_rm_data << dp_reg_operand }
+			case 1: { dp_reg_rm_data = dp_reg_rm_data >> dp_reg_operand }
+			case 2: { dp_reg_rm_data = (((int32_t)dp_reg_rm_data) >> dp_reg_operand) }
+		}
 
+	        if (GET_BIT(dp_reg_opr, 0) == 0) {
+		    // arithmetic
+		    // same as for dp_imm
+		    
+		    arith_instr_exec(dp_reg_opc, dp_reg_rd, dp_reg_rn_data, dp_reg_rm_data);
+		    
+
+		} else {
+		    // logical
+		    // handle shift case 11
+		    // handle case of N = 0,1
+		    // logical instructions
+		    if (dp_reg_shift == 3) {
+			uint32_t right_shift = dp_reg_rm_data >> dp_reg_operand;
+    			uint32_t rotate_bits = dp_reg_rm_data << (32-dp_reg_operand);
+    			dp_reg_rm_data  = right_shift | rotate_bits;
+		    }
+
+		    if (GET_BIT(dp_reg_opr, 3) == 1) {
+			    NEGATE_MASK(dp_reg_rm_data);
+		    }
+
+		    switch (dp_reg_opc) {
+			    case 0: {
+				uint32_t res = dp_reg_rn_data & dp_reg_rm_data;
+				break;
+			    }
+			    case 1: {
+				uint32_t res = dp_reg_rn_data | dp_reg_rm_data;
+				break;
+			    }
+			    case 2: {
+				uint32_t res = dp_reg_rn_data ^ dp_reg_rm_data;
+				break;
+			    }
+			    case 3: {
+				uint32_t res = dp_reg_rn_data & dp_reg_rm_data;
+				// set flags
+		    }
+		    write_general_registers(dp_reg_rd, res);
+		}		
+	    } else {
+	        // multiply
+	    }
+		   
             break;
         }
         case SINGLE_DATA_TRANSFER: {
