5e9644875c1cd15f47d07fb15e6044d5ecffda68
Lawan Fathullah <lf523@ic.ac.uk>
2024-05-31 17:25:17 +0100

    fixed syntax errors and cleaned up in fetch.*

--- src/fetch.c
+++ src/fetch.c
@@ -1,17 +1,16 @@
-#include <stdint.h>
 #include <assert.h>
-#include "registers.h"
+#include "fetch.h"
 
 /*
     A function that takes the address held in the Program Counter
     and returns the Instruction held at that address
 */
-
 uint32_t fetch(MachineState *machine_state) {
     assert(machine_state != NULL);
+
     // Reads the address held in the Program Counter
     Register pc = machine_state->program_counter;
-    uint32_t pc_address = pc.data; // only takes the lower bits
+    uint32_t pc_address = pc.data; // Only takes the lower bits
     uint32_t instruction = readmem32(pc_address);
 
     // Return the instruction
--- src/fetch.h
+++ src/fetch.h
@@ -1,4 +1,9 @@
+#ifndef FETCH_H
+#define FETCH_H
 #include <stdint.h>
+#include "memory.h"
 #include "registers.h"
 
-extern uint32_t fetch(MachineState *machine_state)
+extern uint32_t fetch(MachineState *machine_state);
+
+#endif
