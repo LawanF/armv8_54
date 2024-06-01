06025db273424b79ad936f5faaed614944264c2a
Rukaiya Khondaker <rnk23@ic.ac.uk>
2024-06-01 21:31:44 +0100

    Added the write_general_registers function and corrected machine state initialisation

--- src/registers.c
+++ src/registers.c
@@ -1,39 +1,52 @@
 #include "registers.h"
+#include <assert.h>
+#include <stdio.h>
 
 static MachineState machine_state;
 
 void init_machine_state(void) {
+    // Creating a pointer to the machine state to alter the global var
+    MachineState *ms = &machine_state;
+
     // Initialising the general registers
-    Register reg_arr[NUM_GENERAL_REGISTERS];
     for (int i = 0; i < NUM_GENERAL_REGISTERS; i++) {
-        Register current_reg;
-        current_reg.writable = 1;
-        current_reg.data = 0;
-        reg_arr[i] = current_reg;
+        ms->general_registers[i].data = 0;
+        ms->general_registers[i].writable = 1;
     }
 
-    // Initialising the zero register
-    Register zero;
-    zero.writable = 0;
-    zero.data = 0;
+    // Initialising the special registers
+    ms->zero_register.data = 0;
+    ms->zero_register.writable = 0;
 
-    // Initialising the program counter
-    Register pc;
-    pc.writable = 1;
-    pc.data = 0;
+    ms->program_counter.data = 0;
+    ms->program_counter.writable = 1;
 
-    // Initialising the processor state register
-    ProcessorStateRegister pstate_reg;
-    pstate_reg.zero = 0;
-    pstate_reg.neg = 0;
-    pstate_reg.carry = 0;
-    pstate_reg.overflow = 0;
-
-    // Putting all the registers into the machine state
-    machine_state.general_registers = reg_arr;
-    machine_state.zero_register = zero;
-    machine_state.program_counter = pc;
-    machine_state.pstate = pstate_reg;
+    ms->pstate.zero = 0;
+    ms->pstate.neg = 0;
+    ms->pstate.carry = 0;
+    ms->pstate.overflow = 0;
 }
 
-void 
+/*
+    A function that returns a copy of the machine_state (meant to be
+    read-only)
+*/
+MachineState read_machine_state(void) {
+    return machine_state;
+}
+
+
+/*
+    A function that writes to a specific general register given
+    the required index, and the value that needs to be written
+*/
+void write_general_registers(int index, uint64_t value) {
+    // Check that the index refers to an existing general register
+    assert(index <= NUM_GENERAL_REGISTERS);
+    assert(index >= 0);
+
+    // Find the general register and write
+    MachineState *ms = &machine_state;
+    ms->general_registers[index].data = value; 
+}
+
