3da7e95eb8b23b180703c2170771c3cff6464596
Mohammad A <ma3523@ic.ac.uk>
2024-06-03 11:51:39 +0100

    added bucket struct and changed int to use stdint.h in symbol_table.c

--- src/symbol_table.c
+++ src/symbol_table.c
@@ -1,7 +1,15 @@
 /* Implementation for a symbol table.
  * This is a (hash)map from labels (strings, aka char[]) to memory addresses (unsigned integers). */
 
+#include <stdint.h>
+
 typedef struct {
     char *label;
-    int address;
+    uint32_t address;
 } MapEntry;
+
+typedef struct {
+    MapEntry *entries;
+    uint16_t size;
+    uint16_t capacity;
+} MapBucket;
