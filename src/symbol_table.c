c1c157903bf150164bc4356ec1416b844749c517
Mohammad A <ma3523@ic.ac.uk>
2024-06-03 16:28:51 +0100

    renamed MapEntry and MapBucket to remove "Map" and added SymbolTable in symbol_table.c

--- src/symbol_table.c
+++ src/symbol_table.c
@@ -1,40 +1,43 @@
 /* Implementation for a symbol table.
  * This is a (hash)map from labels (strings, aka char[]) to memory addresses (unsigned integers). */
 
-#include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 
+/**
+ * A structure representing an entry in a bucket.
+ * @property label a copy of the label inserted into the bucket
+ * @property address the address in memory this label maps to
+ */
 typedef struct {
-    const char *label;
+    char *label;
     uint32_t address;
-} MapEntry;
+} Entry;
 
-// A resizing array of map entries.
-typedef struct {
-    MapEntry *entries;
-    uint16_t size;
-    uint16_t capacity;
-} MapBucket;
+/**
+ * A structure representing a resizing array of map entries.
+ * @property head a pointer to the first of `size` entries in the bucket
+ * @property size the current size of the bucket
+ */
+struct bucket;
+typedef struct bucket *Bucket;
+struct bucket {
+    Entry entry;
+    Bucket tail;
+};
 
-/* A resizing array of buckets, that attempts to resize when the number of entries exceeds the product
- * of the number of buckets and the load factor (the maximum average number of entries per bucket).
+/**
+ * A structure representing a resizing array of buckets.
+ * The array attempts to resize when the number of entries exceeds the product of the number of buckets
+ * and the load factor (the maximum average number of entries per bucket).
  * The maximum number of buckets is 2^16 (65536), and the number of buckets should always be a power of two
- * for hashing to work properly. */
-typedef struct {
+ * for hashing to work properly.
+ * @property load_factor the maximum allowed average number of entries per bucket
+ * @property buckets a basal pointer to an array of `Bucket`s
+ * @property num_buckets the current number of buckets
+ */
+typedef struct symtable {
     const float load_factor;
-    MapBucket *buckets;
+    Bucket *buckets;
     uint16_t num_buckets;
-} SymbolTable;
-
-// Creates an empty bucket with no entries, and an initial capacity of one entry.
-// Returns a pointer to the bucket if creation succeeded, and NULL if it failed.
-MapBucket *create_bucket(void) {
-    MapEntry *entries = malloc(sizeof(MapEntry));
-    if (entries == NULL) return NULL;
-    MapBucket *bucket_ptr = malloc(sizeof(MapBucket));
-    if (bucket_ptr == NULL) return NULL;
-    *bucket_ptr = (MapBucket) { .entries = entries, .size = 0, .capacity = 1 };
-    return bucket_ptr;
-}
-
-// Creates an empty symbol table with one bucket, and an initial maximum capacity
+} *SymbolTable;
