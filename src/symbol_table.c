05beb855f0c45c0a8a648e1b9638432541a5f9a9
Mohammad A <ma3523@ic.ac.uk>
2024-06-03 11:48:34 +0100

    created map entry struct in symbol_table.c

--- src/symbol_table.c
+++ src/symbol_table.c
@@ -1,2 +1,7 @@
 /* Implementation for a symbol table.
  * This is a (hash)map from labels (strings, aka char[]) to memory addresses (unsigned integers). */
+
+typedef struct {
+    char *label;
+    int address;
+} MapEntry;
