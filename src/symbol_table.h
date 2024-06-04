761e2f31cad4db3c024c9b4151287571304f74ed
Mohammad A <ma3523@ic.ac.uk>
2024-06-04 13:01:25 +0100

    created symbol_table.h and moved definition of SymbolTable from symbol_table.c

---
+++ src/symbol_table.h
@@ -0,0 +1,20 @@
+/** A module for creating symbol tables (hash maps from labels (of type char *) to addresses (of type uint16_t)).
+ * The hash map is dynamically allocated.
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct symtable *SymbolTable;
+
+SymbolTable symtable_new(float load_factor);
+
+void symtable_free(SymbolTable symtable);
+
+bool symtable_contains(SymbolTable symtable, const char *key);
+
+bool symtable_find(SymbolTable symtable, const char *key, uint16_t *dest);
+
+bool symtable_remove(SymbolTable symtable, const char *key, uint16_t *dest);
+
+bool symtable_set(SymbolTable symtable, const char *key, const uint16_t address);
