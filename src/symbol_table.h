8e81fa0e68a57f35da63f23e085cdfa9907ba6de
Mohammad A <ma3523@ic.ac.uk>
2024-06-06 12:55:27 +0100

    modified function templates in symbol_table.h

--- src/symbol_table.h
+++ src/symbol_table.h
@@ -17,8 +17,10 @@ bool symtable_get(SymbolTable symtable, const char *key, uint32_t *dest);
 
 bool single_symtable_remove(SymbolTable symtable, const char *key, uint32_t *dest);
 
-bool multi_symtable_remove(SymbolTable symtable, const char *key, uint32_t *dest);
-
 bool single_symtable_set(SymbolTable symtable, const char *key, const uint32_t address);
 
 bool multi_symtable_add(SymbolTable symtable, const char *key, const uint32_t address);
+
+bool multi_symtable_remove_first(SymbolTable symtable, const char *key, uint32_t *dest);
+
+bool multi_symtable_remove_all(SymbolTable symtable, const char *key, uint32_t *dest);
