a85b1786b8c69652340dac48d266f86f01597e45
Mohammad A <ma3523@ic.ac.uk>
2024-06-13 12:25:34 +0100

    removed memory address check when writing to PC in registers.c

--- src/emulate_files/registers.c
+++ src/emulate_files/registers.c
@@ -58,7 +58,6 @@ void write_general_registers(int index, uint64_t value) {
     the logic of what that might be is  dealt with separately
 */
 void write_program_counter(uint32_t address) {
-    check_address_boundary(address);
     MachineState *ms_pointer = &machine_state;
     ms_pointer->program_counter.data = address;
 }
