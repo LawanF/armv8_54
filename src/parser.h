4a3da769798848cde056a8dd0abe9212c57e669d
Mohammad A <ma3523@ic.ac.uk>
2024-06-13 12:16:02 +0100

    modified definitions of mnemonic arrays to be constants in parser.h

--- src/parser.h
+++ src/parser.h
@@ -5,9 +5,9 @@
 #define NUM_BRANCH_INSTS 9
 #define NUM_SDT_INSTS 2
 
-char *str_dp_imm[] = {"add", "adds", "and", "ands", "bic", "bics", "cmn", "cmp", "eon", "eor", "mov", "movk", "movn", "movz", "mvn", "neg", "negs", "orn", "orr", "sub", "subs", "tst"};
-char *str_dp_reg[] = {"madd", "mneg", "msub", "mul"};
-char *str_branch[] = {"b", "b.al", "b.eq", "b.ge", "b.gt", "b.le", "b.lt", "b.ne", "br"};
-char *str_sdt[] = {"ldr", "str"};
+const char *const str_dp_imm[] = {"add", "adds", "and", "ands", "bic", "bics", "cmn", "cmp", "eon", "eor", "mov", "movk", "movn", "movz", "mvn", "neg", "negs", "orn", "orr", "sub", "subs", "tst"};
+const char *const str_dp_reg[] = {"madd", "mneg", "msub", "mul"};
+const char *const str_branch[] = {"b", "b.al", "b.eq", "b.ge", "b.gt", "b.le", "b.lt", "b.ne", "br"};
+const char *const str_sdt[] = {"ldr", "str"};
 
 #endif
