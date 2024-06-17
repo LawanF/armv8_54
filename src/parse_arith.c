d1d1fc7325055604171e16fef7410a3f1feba4dd
Mohammad A <ma3523@ic.ac.uk>
2024-06-17 14:17:44 +0100

    added function for parsing arithmetic register operand in parse_arith.c

--- src/parse_arith.c
+++ src/parse_arith.c
@@ -41,3 +41,37 @@ static bool parse_arith_imm_op2(
     *instruction = inst;
     return true;
 }
+
+
+/** Parses the register form of an arithmetic < op2 > operand, a
+ * string of the form "Rn{, [shift] #[imm]}", and fills in the corresponding
+ * Note that the opcode and register (rn) for this instruction will need to be
+ * set separately.
+ * @returns true if and only if the operand matches, and parsing succeeds.
+ */
+static bool parse_arith_reg_op2(
+    char **src,
+    uint8_t rn,
+    Instruction *instruction
+) {
+    Instruction inst = *instruction;
+    char *s = *src;
+    RegisterWidth rm_width;
+    ShiftType shift_type = 0;
+    uint8_t shift_amount = 0;
+    uint8_t rm;
+    bool success = parse_reg(&s, &rm, &rm_width)
+                && (rm_width == inst.sf);
+    if (!success) return false;
+    // parse optional shift
+    parse_immediate_shift(&s, &shift_type, &shift_amount);
+    inst.command_format = DP_REG;
+    // For an arithmetic instruction, (M, opr) if of the form (0, 1xx0)
+    // where the xx bits form opr.
+    inst.dp_reg.m = 0;
+    inst.dp_reg.opr = 4 | (shift_type << 1); // (m, opr) = (0, 1xx0)
+    inst.dp_reg.rn = rn;
+    inst.dp_reg.rm = rm;
+    inst.dp_reg.operand = shift_amount;
+    return true;
+}
