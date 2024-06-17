9d82fdeabfbf607882979e874aae7c7a1abc6a9e
Sani <sa4523@imperial.ac.uk>
2024-06-17 22:53:43 +0100

    implemented main assemble loop

--- src/assemble.c
+++ src/assemble.c
@@ -18,8 +18,33 @@ int main(int argc, char **argv) {
     // loop read each line until EOF. parse each line, pass to encode, pass to binary file writer.
     // where does two pass come in
     char buffer[20];
-    while ( fgets(buffer, sizeof(buffer), in) != NULL) {
-        parse_ins
+    uint32_t cur_pos = 0;
+
+    SymbolTable known_table = symtable_new(2);
+    SymbolTable unknown_table = symtable_new(2);
+
+    Instruction instructions[100];
+    char **src;
+    bool is_valid;
+    while ( fgets(buffer, sizeof(buffer), in) != NULL ) {
+        // ADD: resizing array for instructions
+        src = buffer;
+        Instruction inst;
+        is_valid = parse_instruction(src, &inst, cur_pos, known_table, unknown_table); 
+        if (!(is_valid)) {
+            fprintf(stderr, "invalid instruction");
+            return 1;
+        }
+        instructions[cur_pos] = inst;
+        cur_pos++;
+    }
+
+    for (int i = 0; i < sizeof(instructions); i++) {
+        write_binary_instruction(&inst, output_file);
+    }
+
+    symtable_free(known_table);
+    symtable_free(unknown_table);
 
     return EXIT_SUCCESS;
 }
