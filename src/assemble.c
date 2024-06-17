d35ba4a8a2f103637fd2674f3ef8d69b40c7bcaa
Sani <sa4523@imperial.ac.uk>
2024-06-17 17:12:46 +0100

    output / input file recognition and comments

--- src/assemble.c
+++ src/assemble.c
@@ -1,5 +1,23 @@
 #include <stdlib.h>
 
+static char *input_file = NULL;
+static char *output_file = NULL;
+
 int main(int argc, char **argv) {
-  return EXIT_SUCCESS;
+
+    if (!(argc == 3)) {
+        fprintf(stderr, "usage: ./assemble [input_file] [output_file]");
+        exit(1);
+    }
+
+    output_file = argv[2];
+    input_file = argv[1];
+
+    // initialise if there is anything?
+
+
+    // loop read each line until EOF. parse each line, pass to encode, pass to binary file writer.
+    // where does two pass come in
+
+    return EXIT_SUCCESS;
 }
