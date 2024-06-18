65ae764752caad0ec88b96c3f09ba2201673d145
Mohammad A <ma3523@ic.ac.uk>
2024-06-18 07:59:10 +0100

    added include guard to symbol_table.h

--- src/symbol_table.h
+++ src/symbol_table.h
@@ -1,3 +1,6 @@
+#ifndef SYMBOL_TABLE_H
+#define SYMBOL_TABLE_H
+
 /** A module for creating symbol tables (hash maps from labels (of type char *) to addresses (of type uint32_t)).
  * The hash map is dynamically allocated.
  */
@@ -26,3 +29,5 @@ bool multi_symtable_add(SymbolTable symtable, const char *key, const uint32_t ad
 bool multi_symtable_remove_last(SymbolTable symtable, const char *key, uint32_t *dest);
 
 bool multi_symtable_remove_all(SymbolTable symtable, const char *key, uint32_t *dest);
+
+#endif
