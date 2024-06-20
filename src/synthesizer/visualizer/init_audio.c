f7c832a9de15bbe7939a540a1b6c16221670f76e
Mohammad A <ma3523@ic.ac.uk>
2024-06-21 00:51:56 +0100

    added fullscreen invisible window to init_audio.c

--- src/synthesizer/visualizer/init_audio.c
+++ src/synthesizer/visualizer/init_audio.c
@@ -1,30 +1,32 @@
+#include <math.h>
 #include "init_audio.h"
 #include "constants.h"
-#include "../headers/keyboard.h"
 #include "../headers/format.h"
 #include "../headers/waveforms.h"
 
-SDL_Window *audio_window = NULL;
+static SDL_Window *audio_window = NULL;
+
+SDL_Window *get_audio_window(void) {
+    return audio_window;
+}
 
 static float volume = 1;
+
+float fsin(float phase, float freq) {
+    return sin(2 * M_PI * (phase * freq / SAMPLE_RATE));
+}
 
 void default_callback(void *userdata, Uint8 *stream, int len) {
     static double phase = 0;
 
     for (int i = 0; i < len; i++) {
-        stream[i] = 0;
-        for (int j = 0; j < keyboard_length; j++) {
-            if (get_pressed(j)) {
-                stream[i] += volume * DYNAMIC_RANGE * (fsin)(phase, BASE_FREQ * pow(2, (float) j / 12.0));
-                printf("stream: %d\n", stream[i]);
-            }
-        }
+        stream[i] = volume * DYNAMIC_RANGE * (fsin)(phase, BASE_FREQ * pow(2, (float) 1 / 12.0));
         phase++;
     }
 }
 
 void init_audio(void) {
-    if (SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO) < 0) {
+    if (SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_EVENTS) < 0) {
         PRINT_SDL_ERR("Initialise SDL");
         exit(EXIT_FAILURE);
     }
@@ -32,14 +34,16 @@ void init_audio(void) {
             /* title = */ "Visualizer",
             /* x = */ SDL_WINDOWPOS_CENTERED,
             /* y = */ SDL_WINDOWPOS_CENTERED,
-            /* w = */ WINDOW_WIDTH,
-            /* h = */ WINDOW_HEIGHT,
-            /* flags = */ SDL_WINDOW_HIDDEN);
+            /* w = */ 0,
+            /* h = */ 0,
+            /* flags = */ SDL_WINDOW_FULLSCREEN_DESKTOP);
     if (audio_window == NULL) {
         PRINT_SDL_ERR("Create SDL audio engine");
         exit(EXIT_FAILURE);
     }
     SDL_UpdateWindowSurface(audio_window);
+    SDL_SetWindowOpacity(audio_window, 0);
+
     // Specify audio format.
     SDL_AudioSpec want, have;
     SDL_memset(&want, 0, sizeof(want));
@@ -52,6 +56,12 @@ void init_audio(void) {
     // Open the device.
     SDL_AudioDeviceID device_id = SDL_OpenAudioDevice(NULL, 0, &want, &have, 0);
     if (!device_id) {
-        fprintf(stderr, "SDL_OpenAudioDevice error: %s\n", SDL_GetError());
+        PRINT_SDL_ERR("Obtain free device");
+        exit(EXIT_FAILURE);
     }
+    SDL_PauseAudioDevice(device_id, 0);
+}
+
+void set_volume(float value) {
+    volume = (float) fmin(1.0, fmax(0, value));
 }
