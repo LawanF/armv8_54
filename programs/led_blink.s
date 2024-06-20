1c367186883a015270290ceb3ffbad02ab09bbed
Fathullah, Lawan <lawan.fathullah23@imperial.ac.uk>
2024-06-20 05:36:01 +0100

    Removed system timer from assembly file.

--- programs/led_blink.s
+++ programs/led_blink.s
@@ -1,39 +1,29 @@
-@ Loading values into registers.
 movz w0, #0x0 @ Zero.
 movz w1, #0x3f20, lsl #16 @ GPFSEL memory location.
 movz w2, #0x8000, lsl #12 @ To set pin 9 as output.
 add w3, w1, #0x1c @ GPSET0 memory location.
 add w4, w1, #0x28 @ GPCLR0 memory location.
 movz w5, #0x100 @ To set GPSET and GPCLR for pin 9.
-movz w6, #0x7e00, lsl #16 @ System timer base address.
-add w6, w6, #0x3000
-movz w10, #0x2710 @ Epsilon for time calculation.
 
-@ Setting GPIO pin settings.
 str w2, [w0, w1] @ Set pin 9 as output.
 
-@ Waiting procedure.
 wait1:
-ldr w7, [w6, #0x4] @ Load current time.
-add w8, w7, #0xf424, lsl #4 @ Time offset of one million.
+movz w6, #0x0 @ Set timer to 0.
+movz w7, #0x3b9a, lsl #16 @ Timer target.
 wait1_loop:
-ldr w7, [w6, #0x4] @ Update current time.
-sub w9, w7, w8 @ Calculate time difference.
-cmp w10, w9
-b.ge wait1_loop
+add w6, w6, #0x1
+cmp w6, w7
+br.ne wait1_loop
 
-@ GPSET
 str w5, [w0, w3] @ Set GPSET for pin 9.
 
-@ Waiting procedure.
 wait2:
-ldr w7, [w6, #0x4] @ Load current time.
-add w8, w7, #0xf424, lsl #4 @ Time offset of one million.
+movz w6, #0x0 @ Set timer to 0.
+movz w7, #0x3b9a, lsl #16 @ Timer target.
 wait2_loop:
-ldr w7, [w6, #0x4] @ Update current time.
-sub w9, w7, w8 @ Calculate time difference.
-cmp w10, w9
-b.ge wait2_loop
+add w6, w6, #0x1
+cmp w6, w7
+br.ne wait2_loop
 
-str w5, [w, w4] @ Set GPCLR for pin 9.
+str w5, [w0, w4] @ Set GPCLR for pin 9.
 br wait1 @ Repeat loop.
